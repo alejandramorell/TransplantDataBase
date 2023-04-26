3fa5fa24ccaf3c1d85fdee50abf25d27517d5e41
alejandramorell <alejandra.morellgamboa@usp.ceu.es>
2023-04-26 19:41:21 +0200

    NOTHING

--- src/transplant/pojos/Organ.java
+++ src/transplant/pojos/Organ.java
@@ -1,10 +1,19 @@
 package transplant.pojos;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Organ implements Serializable{
 
 	private static final long serialVersionUID = -3310068065836406234L;
+	private Integer id;
+	private String bloodType;
+	private List<Donor> donors;
+	
+	public Organ() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 	
