3badb399cfbe21bcf3e9ba8f00b69dae874758c3
ainho <ainho@10.60.92.221>
2023-05-10 18:31:15 +0200

    XML start

--- src/transplant/pojos/Donor.java
+++ src/transplant/pojos/Donor.java
@@ -5,17 +5,33 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import javax.xml.bind.annotation.XmlType;
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = { "address", "donor"})
+
 public class Donor implements Serializable{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -6939768601635036985L;
+	@XmlTransient
 	private Integer id;
+	@XmlAttribute
 	private String name;
+	@XmlElement
 	private String adress;
+	@XmlElement
 	private Integer phone;
+	@XmlElement
 	private String livingState;
+	@XmlTransient
 	private List<Organ> organs;
 	
 	public Donor() {
