2e8b37334de45efa54a73ae0b24cc9f8497cf86a
ainho <ainho@10.60.94.8>
2023-05-11 18:31:41 +0200

    changes

---
+++ src/xml/XMLManagerImplementation.java
@@ -0,0 +1,20 @@
+package xml;
+
+import ifaces.XMLManager;
+import transplant.pojos.Organ;
+
+public class XMLManagerImplementation implements XMLManager {
+
+	@Override
+	public String Organ2Xml(Organ organ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Organ xml2Organ(String xml) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
