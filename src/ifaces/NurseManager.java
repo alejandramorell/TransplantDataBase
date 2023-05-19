a22161459380af526a3f693f124590a549df9ff0
daval <daval@10.60.92.133>
2023-05-19 18:10:55 +0100

    changes in nurse

--- src/ifaces/NurseManager.java
+++ src/ifaces/NurseManager.java
@@ -1,14 +1,9 @@
 package ifaces;
 
-import java.util.List;
-
 import transplant.pojos.*;
 
 public interface NurseManager {
 
-	public void insertNurse(Nurse nurse);
-	public List<Nurse> searchNurseByName(String name);
-	public Nurse getNurse(int id);
 	public void assignSurgeonTransplant(int surgeon_id, int transplant_id);
 	public Nurse getNurseByEmail(String email); 
 	
